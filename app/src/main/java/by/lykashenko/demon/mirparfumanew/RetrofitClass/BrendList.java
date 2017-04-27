ad4ba59822c477d44941537b964904388dd2758a
Demonius <demonius2011@gmail.com>
2017-04-28 02:56:03 +0300

    Revert "add constructor for child and sections for list brend"
    This reverts commit 72b9f2a8e7ab5d884007e2b4c46120289e6ff3a6.

--- app/src/main/java/by/lykashenko/demon/mirparfumanew/RetrofitClass/BrendList.java
+++ app/src/main/java/by/lykashenko/demon/mirparfumanew/RetrofitClass/BrendList.java
@@ -21,56 +21,56 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class BrendList {
 
 
-    public interface OnLoadBrendList {
-        void onLoadBrendList(ArrayList<Brendu> brendu);
-    }
+ public interface OnLoadBrendList {
+  void onLoadBrendList(ArrayList<Brendu> brendu);
+ }
 
-    private OnLoadBrendList brendList;
+ private OnLoadBrendList brendList;
 
-    public void registerOnLoadBrendList(OnLoadBrendList callback) {
-        this.brendList = callback;
-    }
-
-
-    private Context context;
+ public void registerOnLoadBrendList(OnLoadBrendList callback) {
+  this.brendList = callback;
+ }
 
 
-    public BrendList(Context context) {
-        this.context = context;
-    }
+ private Context context;
 
 
-    public void load(String sql_string_limit) {
+ public BrendList(Context context) {
+  this.context = context;
+ }
 
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(MainActivity.URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+ public void load(String sql_string_limit) {
 
-        InterfaceLoadBrendu loadBrendu = retrofit.create(InterfaceLoadBrendu.class);
-        Call<ArrayList<Brendu>> call = loadBrendu.load(sql_string_limit);
-        call.enqueue(new Callback<ArrayList<Brendu>>() {
-            @Override
-            public void onResponse(Call<ArrayList<Brendu>> call, Response<ArrayList<Brendu>> response) {
-                if (response != null) {
-                    ArrayList<Brendu> brendu = response.body();
+
+  Retrofit retrofit = new Retrofit.Builder()
+    .baseUrl(MainActivity.URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build();
+
+  InterfaceLoadBrendu loadBrendu = retrofit.create(InterfaceLoadBrendu.class);
+  Call<ArrayList<Brendu>> call = loadBrendu.load(sql_string_limit);
+call.enqueue(new Callback<ArrayList<Brendu>>() {
+ @Override
+ public void onResponse(Call<ArrayList<Brendu>> call, Response<ArrayList<Brendu>> response) {
+  if (response!=null){
+   ArrayList<Brendu> brendu = response.body();
 //   Log.i(MainActivity.LOG_TAG,"Количество брендов => "+brendu.size());
-                    brendList.onLoadBrendList(brendu);
+   brendList.onLoadBrendList(brendu);
 
-                } else {
-                    Log.e(MainActivity.LOG_TAG, "ошибка ответа: код => " + response.code() + " ,тело ошибки => " + response.message());
-                }
-            }
+  }else{
+   Log.e(MainActivity.LOG_TAG,"ошибка ответа: код => "+response.code()+" ,тело ошибки => "+response.message());
+  }
+ }
 
-            @Override
-            public void onFailure(Call<ArrayList<Brendu>> call, Throwable t) {
-                Log.e(MainActivity.LOG_TAG, "ошибка азпроса => " + t.getMessage());
-            }
-        });
+ @Override
+ public void onFailure(Call<ArrayList<Brendu>> call, Throwable t) {
+Log.e(MainActivity.LOG_TAG,"ошибка азпроса => "+t.getMessage());
+ }
+});
 
 
-    }
+ }
 
 
 }
